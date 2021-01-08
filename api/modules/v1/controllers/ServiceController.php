00a16d7791214387fe2224709f66222de10a8e83
ayhamJaradat <ayham.jaradat@exalt.ps>
2021-01-08 10:53:07 +0200

    create update user API

--- api/modules/v1/controllers/ServiceController.php
+++ api/modules/v1/controllers/ServiceController.php
@@ -10,6 +10,8 @@ namespace api\modules\v1\controllers;
 
 use api\modules\v1\resources\GeneralResponse;
 use api\modules\v1\resources\SignupForm;
+use api\modules\v1\resources\UpdateUserForm;
+use common\models\User;
 use yii\base\Exception;
 use yii\filters\Cors;
 use yii\helpers\ArrayHelper;
@@ -54,10 +56,12 @@ class ServiceController extends Controller
 
     public function actionSayHi(){
         return "Hi from Shabbik services";
+//        $user = User::find()->one();
+//        return $user;
     }
 
     /**
-     * @return GeneralResponse
+     * @return object
      * @throws Exception
      * @throws \yii\base\InvalidConfigException
      */
@@ -70,17 +74,54 @@ class ServiceController extends Controller
         // Set vars
         $request = Yii::$app->getRequest();
         $model = new SignupForm();
-        $response = new GeneralResponse();
+//        $response = new GeneralResponse();
+
 
         // Load model
         $model->setAttributes($request->getBodyParams());
         $results = $model->signup();
         if ($results ) {
-            $response->data = $results;
-            $response->status = true;
+//            $response->data = $results;
+//            $response->status = true;
+            $response = (object)array('status'=>true, 'userId'=>$results->id);
         }else{
-            $response->errors = $model->errors;
-            $response->status = false;
+//            $response->errors = $model->errors;
+//            $response->status = false;
+            $response = (object)array('status'=>false, 'errorMsg'=>"could not add this user");
+
+        }
+        return $response;
+    }
+
+
+    /**
+     * @return object
+     * @throws Exception
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionUpdateUser()
+    {
+        if(!\Yii::$app->request->isPost)
+        {
+            throw new Exception("Only Post request is allowed");
+        }
+        // Set vars
+        $request = Yii::$app->getRequest();
+        $model = new UpdateUserForm();
+//        $response = new GeneralResponse();
+
+
+        // Load model
+        $model->setAttributes($request->getBodyParams());
+        $results = $model->updateUser();
+        if ($results ) {
+//            $response->data = $results;
+//            $response->status = true;
+            $response = (object)array('status'=>true, 'userId'=>$results->id);
+        }else{
+//            $response->errors = $model->errors;
+//            $response->status = false;
+            $response = (object)array('status'=>false, 'errorMsg'=>"user is not registered");
 
         }
         return $response;
